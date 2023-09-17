8571456a9e30082ed535c511fe838b9f7e32794a
HamzaGorcevic <gorcevichamza76@gmail.com>
2023-09-17 15:41:48 +0200

    exercise medium

--- index.js
+++ index.js
@@ -481,21 +481,153 @@
 // }
 
 // console.log(longestConsecutive([1, 2, 0, 1]));
-function isValid(s) {
-    const stack = [];
-    let combination = { "(": ")", "{": "}", "[": "]" };
-    let combination2 = { ")": 1, "}": 2, "]": 3 };
+// function isValid(s) {
+//     const stack = [];
+//     let combination = { "(": ")", "{": "}", "[": "]" };
+//     let combination2 = { ")": 1, "}": 2, "]": 3 };
 
-    for (let i = 0; i < s.length; i++) {
-        if (combination[s[i]]) {
-            stack.push(combination[s[i]]);
-        } else if (combination2[s[i]]) {
-            if (stack.pop() != s[i]) {
-                return false;
-            }
-        }
-    }
-    return true;
+//     for (let i = 0; i < s.length; i++) {
+//         if (combination[s[i]]) {
+//             stack.push(combination[s[i]]);
+//         } else if (combination2[s[i]]) {
+//             if (stack.pop() != s[i]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isValid("()"));
+
+function isOperator(token) {
+    return token === "+" || token === "-" || token === "*" || token === "/";
 }
+function evalRPN(tokens) {
+    const stack = [];
+    let counted = "";
+    for (let i = 0; i < tokens.length; i++) {
+        if (isOperator(tokens[i])) {
+            let temp = stack.pop();
+            counted = eval(
+                "(" + stack.pop() + ")" + tokens[i] + "(" + temp + ")"
+            );
+            stack.push(counted);
+            console.log("c:", counted);
+            counted = "";
+            continue;
+        }
 
-console.log(isValid("()"));
+        stack.push(tokens[i]);
+    }
+    return parseInt(stack.pop());
+}
+console.log(
+    evalRPN([
+        "-78",
+        "-33",
+        "196",
+        "+",
+        "-19",
+        "-",
+        "115",
+        "+",
+        "-",
+        "-99",
+        "/",
+        "-18",
+        "8",
+        "*",
+        "-86",
+        "-",
+        "-",
+        "16",
+        "/",
+        "26",
+        "-14",
+        "-",
+        "-",
+        "47",
+        "-",
+        "101",
+        "-",
+        "163",
+        "*",
+        "143",
+        "-",
+        "0",
+        "-",
+        "171",
+        "+",
+        "120",
+        "*",
+        "-60",
+        "+",
+        "156",
+        "/",
+        "173",
+        "/",
+        "-24",
+        "11",
+        "+",
+        "21",
+        "/",
+        "*",
+        "44",
+        "*",
+        "180",
+        "70",
+        "-40",
+        "-",
+        "*",
+        "86",
+        "132",
+        "-84",
+        "+",
+        "*",
+        "-",
+        "38",
+        "/",
+        "/",
+        "21",
+        "28",
+        "/",
+        "+",
+        "83",
+        "/",
+        "-31",
+        "156",
+        "-",
+        "+",
+        "28",
+        "/",
+        "95",
+        "-",
+        "120",
+        "+",
+        "8",
+        "*",
+        "90",
+        "-",
+        "-94",
+        "*",
+        "-73",
+        "/",
+        "-62",
+        "/",
+        "93",
+        "*",
+        "196",
+        "-",
+        "-59",
+        "+",
+        "187",
+        "-",
+        "143",
+        "/",
+        "-79",
+        "-89",
+        "+",
+        "-",
+    ])
+);
