ec01634241a1f00ce1c2e5b1bf6baafda266fc0d
HamzaGorcevic <gorcevichamza76@gmail.com>
2023-09-18 20:10:09 +0200

    medium backtracking algorithm

--- index.js
+++ index.js
@@ -500,134 +500,164 @@
 
 // console.log(isValid("()"));
 
-function isOperator(token) {
-    return token === "+" || token === "-" || token === "*" || token === "/";
-}
-function evalRPN(tokens) {
-    const stack = [];
-    let counted = "";
-    for (let i = 0; i < tokens.length; i++) {
-        if (isOperator(tokens[i])) {
-            let temp = stack.pop();
-            counted = eval(
-                "(" + stack.pop() + ")" + tokens[i] + "(" + temp + ")"
-            );
-            stack.push(counted);
-            console.log("c:", counted);
-            counted = "";
-            continue;
-        }
+// function isOperator(token) {
+//     return token === "+" || token === "-" || token === "*" || token === "/";
+// }
+// function evalRPN(tokens) {
+//     const stack = [];
+//     let counted = "";
+//     for (let i = 0; i < tokens.length; i++) {
+//         if (isOperator(tokens[i])) {
+//             let temp = stack.pop();
+//             counted = eval(
+//                 "(" + stack.pop() + ")" + tokens[i] + "(" + temp + ")"
+//             );
+//             stack.push(counted);
+//             console.log("c:", counted);
+//             counted = "";
+//             continue;
+//         }
 
-        stack.push(tokens[i]);
+//         stack.push(tokens[i]);
+//     }
+//     return parseInt(stack.pop());
+// }
+// console.log(
+//     evalRPN([
+//         "-78",
+//         "-33",
+//         "196",
+//         "+",
+//         "-19",
+//         "-",
+//         "115",
+//         "+",
+//         "-",
+//         "-99",
+//         "/",
+//         "-18",
+//         "8",
+//         "*",
+//         "-86",
+//         "-",
+//         "-",
+//         "16",
+//         "/",
+//         "26",
+//         "-14",
+//         "-",
+//         "-",
+//         "47",
+//         "-",
+//         "101",
+//         "-",
+//         "163",
+//         "*",
+//         "143",
+//         "-",
+//         "0",
+//         "-",
+//         "171",
+//         "+",
+//         "120",
+//         "*",
+//         "-60",
+//         "+",
+//         "156",
+//         "/",
+//         "173",
+//         "/",
+//         "-24",
+//         "11",
+//         "+",
+//         "21",
+//         "/",
+//         "*",
+//         "44",
+//         "*",
+//         "180",
+//         "70",
+//         "-40",
+//         "-",
+//         "*",
+//         "86",
+//         "132",
+//         "-84",
+//         "+",
+//         "*",
+//         "-",
+//         "38",
+//         "/",
+//         "/",
+//         "21",
+//         "28",
+//         "/",
+//         "+",
+//         "83",
+//         "/",
+//         "-31",
+//         "156",
+//         "-",
+//         "+",
+//         "28",
+//         "/",
+//         "95",
+//         "-",
+//         "120",
+//         "+",
+//         "8",
+//         "*",
+//         "90",
+//         "-",
+//         "-94",
+//         "*",
+//         "-73",
+//         "/",
+//         "-62",
+//         "/",
+//         "93",
+//         "*",
+//         "196",
+//         "-",
+//         "-59",
+//         "+",
+//         "187",
+//         "-",
+//         "143",
+//         "/",
+//         "-79",
+//         "-89",
+//         "+",
+//         "-",
+//     ])
+// );
+
+// medium zagrade
+
+function generateParenthesis(n) {
+    let stack = [];
+    let niz = [];
+
+    function recursive(open, closed) {
+        console.log(open == closed && closed == n);
+        if (open == closed && closed == n) {
+            console.log("we are in");
+            niz.push(stack.join(""));
+            return;
+        }
+        if (open < n) {
+            stack.push("(");
+            recursive(open + 1, closed);
+            stack.pop();
+        }
+        if (closed < open) {
+            stack.push(")");
+            recursive(open, closed + 1);
+            stack.pop();
+        }
     }
-    return parseInt(stack.pop());
+    recursive(0, 0);
+    return niz;
 }
-console.log(
-    evalRPN([
-        "-78",
-        "-33",
-        "196",
-        "+",
-        "-19",
-        "-",
-        "115",
-        "+",
-        "-",
-        "-99",
-        "/",
-        "-18",
-        "8",
-        "*",
-        "-86",
-        "-",
-        "-",
-        "16",
-        "/",
-        "26",
-        "-14",
-        "-",
-        "-",
-        "47",
-        "-",
-        "101",
-        "-",
-        "163",
-        "*",
-        "143",
-        "-",
-        "0",
-        "-",
-        "171",
-        "+",
-        "120",
-        "*",
-        "-60",
-        "+",
-        "156",
-        "/",
-        "173",
-        "/",
-        "-24",
-        "11",
-        "+",
-        "21",
-        "/",
-        "*",
-        "44",
-        "*",
-        "180",
-        "70",
-        "-40",
-        "-",
-        "*",
-        "86",
-        "132",
-        "-84",
-        "+",
-        "*",
-        "-",
-        "38",
-        "/",
-        "/",
-        "21",
-        "28",
-        "/",
-        "+",
-        "83",
-        "/",
-        "-31",
-        "156",
-        "-",
-        "+",
-        "28",
-        "/",
-        "95",
-        "-",
-        "120",
-        "+",
-        "8",
-        "*",
-        "90",
-        "-",
-        "-94",
-        "*",
-        "-73",
-        "/",
-        "-62",
-        "/",
-        "93",
-        "*",
-        "196",
-        "-",
-        "-59",
-        "+",
-        "187",
-        "-",
-        "143",
-        "/",
-        "-79",
-        "-89",
-        "+",
-        "-",
-    ])
-);
+
+console.log(generateParenthesis(3));
